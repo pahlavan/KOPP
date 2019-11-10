2d62baa2e73f3a2efe8288c868b811e5a737e000
Nima Zolghadr <mazolgha@microsoft.com>
2019-11-10 13:45:18 -0800

    Adjusting detour

--- Assets/Scripts/SpaceshipScript.cs
+++ Assets/Scripts/SpaceshipScript.cs
@@ -23,8 +23,13 @@ public class SpaceshipScript : MonoBehaviour
     void SelectNextHop()
     {
         lastHop = nextHop;
-        var options = nextHop.GetComponent<PlanetScript>().OutgoingPlanets;
-        int cnt = options.Count;
+        var nextHopScript = nextHop.GetComponent<PlanetScript>();
+        var forwardOption = nextHopScript.OutgoingPlanets;
+        var detour = new List<GameObject>();
+        detour.AddRange(nextHopScript.incomingPlanets);
+        detour.AddRange(forwardOption);
+
+        int cnt = forwardOption.Count;
 
         if (cnt == 0)
         {
@@ -33,7 +38,7 @@ public class SpaceshipScript : MonoBehaviour
         }
         else
         {
-            foreach(var planet in options)
+            foreach(var planet in detour)
             {
                 if (planet.GetComponent<PlanetScript>().planetState == PlanetState.Detour)
                 {
@@ -42,7 +47,7 @@ public class SpaceshipScript : MonoBehaviour
                 }
             }
 
-            nextHop = options[Random.Range(0, cnt)];
+            nextHop = forwardOption[Random.Range(0, cnt)];
         }
     }
 
